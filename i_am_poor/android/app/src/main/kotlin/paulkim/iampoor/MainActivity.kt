b69303255f19e6662198269ef74c9c6d7be1df73
Paul Kim <paulracooni@gmail.com>
2020-06-06 19:50:32 +0900

    feat: Setup i-am-poor project

---
+++ i_am_poor/android/app/src/main/kotlin/paulkim/iampoor/MainActivity.kt
@@ -0,0 +1,6 @@
+package paulkim.iampoor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ i_am_poor/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ImPoorApp());
+}
+
+class ImPoorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
