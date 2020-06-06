6eed9a3d56bc8390fc9c5a3b9dde3ca2b6f5bec7
Paul Kim <paulracooni@gmail.com>
2020-06-06 19:24:16 +0900

    feat: Merge master branch

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -2,15 +2,25 @@ import 'package:flutter/material.dart';
 
 // The main fuction is the starting point for all our Flutter apps.
 void main() {
+  String _app_bar_title = 'I Am Rich';
+  String _asset_img = 'res/images/money.png';
+
   runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('I Am Rich'),
           backgroundColor: Colors.blueGrey[900],
+          title: Center(
+              child:Text(_app_bar_title)
+          ),
+        ),
+        body: Center(
+          child:Image(
+            image: AssetImage(_asset_img)
+            ),
+          ),
         ),
       ),
-    ),
   );
 }
