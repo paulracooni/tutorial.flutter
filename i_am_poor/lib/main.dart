7c6f7210aeaae7a2b92da071ad06da58fb95c8da
Paul Kim <paulracooni@gmail.com>
2020-06-06 20:24:38 +0900

    feat: Impl ImPoorApp

--- i_am_poor/lib/main.dart
+++ i_am_poor/lib/main.dart
@@ -5,8 +5,32 @@ void main() {
 }
 
 class ImPoorApp extends StatelessWidget {
+
+  String __app_bar_title = 'I am poor';
+  String __img_street = 'res/images/street.jpg';
+  String __img_bagger = 'res/images/begger.png';
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home:Scaffold(
+        backgroundColor: Colors.blueGrey,
+
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Center(
+              child:Text(__app_bar_title)
+          ),
+        ),
+
+        body:Stack(
+            children: <Widget>[
+              Center(child: Image(image: AssetImage(__img_street))),
+              Center(child: Image(image: AssetImage(__img_bagger))),
+            ]
+          ),
+
+        ),
+      );
   }
 }
