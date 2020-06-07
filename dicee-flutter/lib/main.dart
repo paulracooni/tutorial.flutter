96bb97986f327f1a0c771929aac81cef37796eb7
Paul Kim <paulracooni@gmail.com>
2020-06-07 12:15:44 +0900

    feat: Done class section 7

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,23 +1,78 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  return runApp(
-    MaterialApp(
+  return runApp(DiceApp());
+}
+
+class DiceApp extends StatefulWidget {
+  @override
+  _DiceAppState createState() => _DiceAppState();
+}
+
+class _DiceAppState extends State<DiceApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Text('Dicee')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
       ),
-    ),
-  );
-}
-
-class DicePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+    );
   }
 }
+
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  final _random = new Random();
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset("images/dice$leftDiceNumber.png"),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset("images/dice$rightDiceNumber.png"),
+              ),
+            ),
+          ],
+        ),
+        onPressed: () {
+          rollTheDice();
+        },
+      ),
+    );
+  }
+
+  void rollTheDice(){
+    setState(() {
+      leftDiceNumber = getRandomDiceNum();
+      rightDiceNumber = getRandomDiceNum();
+    });
+  }
+  int getRandomDiceNum(){
+    return 1 + _random.nextInt(6 - 1);
+  }
+}
+
